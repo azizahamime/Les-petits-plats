6f98b65955e76853e44e63288cfd22950acd13c4
Aziza H <mess.aziza@gmail.com>
2022-11-09 09:30:29 +0100

    installation bootstrap

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import "../scss/style.scss";
+import "bootstrap";
 console.log("hello");
--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,30 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname,"dist")
   },
+  stats: {
+    errorDetails: true
+
+  },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader","css-loader","sass-loader"]
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [ "autoprefixer"]
+              }
+            }
+          },
+          "sass-loader"
+        ]
       },
       {
         test: /\.js$/,
