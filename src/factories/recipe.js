19e0f594a26dce2363e08f08e2bcbc20e887f22c
Aziza H <mess.aziza@gmail.com>
2022-11-10 10:19:01 +0100

    recuperer les données

---
+++ src/factories/recipe.js
@@ -0,0 +1,59 @@
+export function recipeFactory(data) {
+  const {name, time, ingredients, description} = data;
+  function getRecipeCard() {
+
+    //creer la colonne
+    const column = document.createElement("div");
+    column.classList.add("col-4");
+
+    //creer figure qui contient le contenu de la recette
+    const figure = document.createElement("figure");
+    figure.classList.add("rounded");
+    column.append(figure);
+
+    //creer la baniére de la recette
+    const photo = document.createElement("div");
+    photo.classList.add("photo w-100");
+    figure.append(photo);
+
+    // creer le contenu de la recette
+    const figcaption = document.createElement("figcaption");
+    const recipeHeader = document.createElement("div");
+    recipeHeader.classList.add("recipe_header p3 d-flex");
+    //ajouter le titre
+    const recipeTitle = document.createElement("h4");
+    recipeTitle.classList.add("recipe_title");
+    recipeTitle.innerHTML = `${name}`;
+    recipeHeader.append(recipeTitle);
+    //ajouter le time
+    const timer = document.createElement("div");
+    timer.innerHTML = `
+          <i class="fa-regular fa-clock"></i>
+          <span class="recipe_time"> ${time} </span>
+    `;
+    recipeHeader.append(timer);
+    figcaption.append(recipeHeader);
+    //contenu recette
+    const descriptionContent = document.createElement("div");
+    descriptionContent.classList.add("description p-3 d-flex");
+    figcaption.append("descriptionContent");
+    //container ingredient
+    const recipeIngredients = document.createElement("div");
+    recipeIngredients.classList.add("recipe_ingredient w-50");
+    ingredients.forEach(ingredient => {
+      const recipeIng = document.createElement("p");
+      recipeIng.innerHTML = `
+        <span class="ingredient_title">${ingredient.ingredient}:</span> ${ingredient.quantity} ${ingredient.unit ? ingredient.unit:"" } 
+      `; 
+      recipeIngredients.append(recipeIng);
+    });
+    descriptionContent.append(recipeIngredients);
+    //container recette description
+    const recipeDescription = document.createElement("div");
+    recipeDescription.classList.add("recipe_description");
+    recipeDescription.innerHTML = `${description}`;
+    description.append(recipeDescription);
+
+  }
+  return {getRecipeCard}
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import "../scss/style.scss";
 import "bootstrap";
 import "../node_modules/axios/dist/axios.min.js";
-function getDonnees() {
-  axios.get("../data/recipes.js").then((res)=>console.log(res.data)).catch((error)=>console.log(error));
-  
+import { recipeFactory } from "./factories/recipe.js";
+
+async function getDatas() {
+  let data =  axios.get("../data/recipes.js").then((el)=> el.data);
+  /*.then((res)=>JSON.parse(res));
+  return data;*/
+  return data;
+}
+
+async function displayRecipe(recipes) {
+  const recipeSection = document.querySelector(".recipes_row");
+  recipes.forEach(rec => {
+    const recipeModel = recipeFactory(rec);
+    const recipeCard = recipeModel.getRecipeCard();
+    recipeSection.append(recipeCard);
+    
+  });
+}
+
+async function init(){
+  const recipes = await getDatas();
+  console.log(recipes);
+  //recipes.forEach(ele =>console.log(ele));
   
 }
-getDonnees();
+init();
