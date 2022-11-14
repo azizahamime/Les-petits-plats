2c1451126d4c56ccbb60938ad9cc67ad1ac65968
Aziza H <mess.aziza@gmail.com>
2022-11-14 10:23:57 +0100

    Amelioration de lalgorithme de recherche

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,18 +1,25 @@
+// importer la fonction displayRecipe
+import { displayRecipe } from "../main.js";
+
+const recipeSection = document.querySelector(".recipes_container");
+const recipeSectionContainer = document.querySelector("#recipes .container");
 /**
  * @param {array} data 
  */
-import { displayRecipe } from "../main.js";
-const recipeSection = document.querySelector(".recipes_container");
-const recipeSectionContainer = document.querySelector("#recipes .container");
 export function searchRecipe(data, word){
-  console.log(data.ingredients);
   let arrayAfterFilter = [];
-  if (String(word).length < 3){
+  // si le nombre de caractére est < 3 afficher toute les recettes
+  if (word.length < 3){
     displayRecipe(data);
   } else {
-    arrayAfterFilter = data.filter(recipe => recipe.name.toLowerCase().includes(word.toLowerCase()));
-    arrayAfterFilter = data.filter(recipe =>recipe.description.toLowerCase().includes(word.toLowerCase()));
-    data.forEach(element => {
+
+    // filtrer les recettes selon le titre ou la description
+    arrayAfterFilter = data.filter(recipe => recipe.name.toLowerCase().includes(word.toLowerCase()) || recipe.description.toLowerCase().includes(word.toLowerCase()) || recipe.ingredients.find(ing =>ing.ingredient.toLowerCase().includes(word.toLowerCase()))  );
+   
+    //arrayAfterFilter = data.filter(recipe =>recipe.description.toLowerCase().includes(word.toLowerCase()));
+
+    //filtrer les recette selon les ingredients
+    /*data.forEach(element => {
       element.ingredients.forEach(ing=>{
         if((ing.ingredient.toLowerCase().includes(word.toLowerCase()))){
           if(!(arrayAfterFilter.includes(element))){
@@ -20,13 +27,14 @@ export function searchRecipe(data, word){
           }
         }
       });
-    });    
+    }); */
     
+    
+    //s'il y a pas une recette qui correspond au caractéres recherchés afficher un message sino afficher les recettes
     if (arrayAfterFilter.length === 0) {
       recipeSectionContainer.innerHTML = `<h2 class="text-center">Aucune recette ne correspond à votre critère… <br> vous pouvez chercher « tarte aux pommes », « poisson », etc.</h2>`;
     } else {
       recipeSection.innerHTML = "";
-      console.log(arrayAfterFilter);             
       displayRecipe(arrayAfterFilter);
 
     }
