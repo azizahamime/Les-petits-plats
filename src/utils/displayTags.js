7c71bf59b817bc834b676fc3aae5bc3cbe8d5246
Aziza H <mess.aziza@gmail.com>
2022-12-07 15:47:08 +0100

    Améliorer l'affichage des listes : ingrédients, ustensils et appareils

--- src/utils/displayTags.js
+++ src/utils/displayTags.js
@@ -1,19 +1,27 @@
 import { searchTags  } from "./searchInTags";
+
 const buttons = document.querySelectorAll(".dropdown button");
 const closes = document.querySelectorAll(".close-dropdown");
+
+//ouvrir le dropdown liste de recherche avancé au click sur les boutons
 buttons.forEach(button =>{
   button.addEventListener("click",function(e){
     this.nextElementSibling.style = "position:relative; top:-55px; left:0;display:block";
     this.style.opacity ="0";
-
   })
-}) 
+})
+
+//Fermer les dropdown liste de recherche avancé au click sur les boutons
 closes.forEach(close =>{
   close.addEventListener("click",function(){
     this.parentNode.parentNode.previousElementSibling.style = "display:relative";
     this.parentNode.parentNode.style ="display:none;";    
   })
 }) 
+
+/**
+ * @param {array} data 
+ */
 export function displaytags(data){
   const ingredientsContainer = document.querySelector(".ingredients");
   const appliancesContainer = document.querySelector(".appliances");
@@ -23,20 +31,25 @@ export function displaytags(data){
   appliancesContainer.innerHTML = "";
   ustensilsContainer.innerHTML = "";
 
-
   const ingArray = [];
   const ustArray = [];
   const applArray = [];
+  
   data.forEach(el=>{
     el.ingredients.forEach(ing => ingArray.push(ing.ingredient));
     el.ustensils.forEach(ust => ustArray.push(ust));
     applArray.push(el.appliance);
   });
   
-  const ings= [...new Set(ingArray.sort())];
-  const ustensilsArr = [...new Set (ustArray.sort())];
-  const applianceArr = [...new Set(applArray.sort())];
+  //eliminer les ingredients doubles
+  const ings= [...new Set((ingArray.map(el=>el.charAt(0).toUpperCase() + el.slice(1))).sort())];
+  //eliminer les ustensils doubles
+  const ustensilsArr = [...new Set ((ustArray.map(el=>el.charAt(0).toUpperCase() + el.slice(1))).sort())];
+  //eliminer les appareils doubles
+  const applianceArr = [...new Set((applArray.map(el=>el.charAt(0).toUpperCase() + el.slice(1))).sort())];
   let plurialArr =[];
+
+  //Enlever le pluriels des ingredients exemple pomme et pommes
   ings.forEach(el=>{
     const plurial = el+"s";
     ings.forEach(ele=>{
@@ -47,19 +60,21 @@ export function displaytags(data){
   })
 
   const ingredientArr = ings.filter(value => !plurialArr.includes(value));
-   
+  // afficher les ingredients dans dropdown liste
   ingredientArr.forEach(ing =>{
     let tagName = document.createElement("li");
     tagName.classList.add('dropdown-item');
     tagName.innerHTML = `${ing}` ;
     ingredientsContainer.append(tagName);
   });
+  // afficher les appareils dans dropdown liste
   applianceArr.forEach(dev =>{
     let tagName = document.createElement("li");
     tagName.classList.add('dropdown-item');
     tagName.innerHTML = `${dev}` ;
     appliancesContainer.append(tagName);
   });
+  // afficher les ustensils dans dropdown liste
   ustensilsArr.forEach(ust =>{
     let tagName = document.createElement("li");
     tagName.classList.add('dropdown-item');
